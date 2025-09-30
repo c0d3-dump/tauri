7e03df59837a75c8f527db8260e792f7bc37393e
c0d3-dump <c0d3.dump@gmail.com>
2025-09-30 15:54:34 +0530

    release pipeline

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,10 +2,11 @@ mod commands;
 mod events;
 mod plugins;
 
-use std::sync::Mutex;
+use std::{sync::Mutex, time::Duration};
 
 use tauri::Listener;
 use tauri_plugin_global_shortcut::ShortcutState;
+use tauri_plugin_updater::UpdaterExt;
 
 #[derive(Debug, Default)]
 struct AppData {
@@ -18,6 +19,19 @@ pub fn run() {
         .plugin(tauri_plugin_updater::Builder::new().build())
         .setup(|app| {
             app.listen("download-started", events::download_started);
+
+            let handle = app.handle().clone();
+            tauri::async_runtime::spawn(async move {
+                loop {
+                    match update(handle.clone()).await {
+                        Ok(_) => println!("Update check completed"),
+                        Err(e) => eprintln!("Update check failed: {}", e),
+                    }
+
+                    // Wait 5 minutes before next check
+                    tokio::time::sleep(Duration::from_secs(600)).await;
+                }
+            });
 
             Ok(())
         })
@@ -32,4 +46,32 @@ pub fn run() {
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
+    println!("update called!");
+
+    if let Some(update) = app.updater()?.check().await? {
+        println!("update available!");
+        let mut downloaded = 0;
+
+        update
+            .download_and_install(
+                |chunk_length, content_length| {
+                    downloaded += chunk_length;
+                    println!("downloaded {downloaded} from {content_length:?}");
+                },
+                || {
+                    println!("download finished!");
+                },
+            )
+            .await?;
+
+        println!("update installed");
+        app.restart();
+    } else {
+        println!("no update required");
+    }
+
+    Ok(())
 }
